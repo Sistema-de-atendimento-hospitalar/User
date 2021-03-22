489cd873b318f19f20445c5d0d2d37dd4ce31a79
stefbitt <stefany.recode01@gmail.com>
2021-03-22 11:13:50 -0300

    user request e updateSenha

--- src/main/java/com/user/user/domain/request/UpdatePassawordRequest.java
+++ src/main/java/com/user/user/domain/request/UpdatePassawordRequest.java
@@ -1,5 +1,14 @@
 package com.user.user.domain.request;
 
 public class UpdatePassawordRequest {
+	private String senha;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
--- src/main/java/com/user/user/domain/request/UserRequest.java
+++ src/main/java/com/user/user/domain/request/UserRequest.java
@@ -1,5 +1,31 @@
 package com.user.user.domain.request;
 
-public class UserRequest {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
+public class UserRequest {
+	@NotNull(message = "Email não pode ser nulo")
+	@Email(message = "Email não é válido")
+	private String email;
+	
+	@NotBlank(message = "Senha não pode está em branco")
+	@NotNull(message = "Senha não pode ser nula")
+	private String senha;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
